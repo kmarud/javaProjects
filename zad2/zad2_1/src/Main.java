837d0c93bbcd1605ec217474adcfe7607deab003
Kamil Marud <200894@student.pwr.edu.pl>
2015-10-21 16:12:01 +0200

    Added new methods in customList

--- zad2/zad2_1/src/Main.java
+++ zad2/zad2_1/src/Main.java
@@ -2,27 +2,24 @@
  * Created by kamil on 20.10.15.
  */
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main{
-    public static void main(String[] args){
-        //tworzymy tablicę
-        List<String> lista = new ArrayList<String>();
+    static void testFuction()
+    {
         MyCustomList<String> sample = new MyCustomList<>();
         //sample.set("aa");
-        System.out.println("przed " + sample.size());
+        //System.out.println("przed " + sample.size());
         sample.add("Ala");
-        sample.add("Ala");
+        //sample.add("Ala");
         sample.add("ma");
-        sample.add("kota");
-        sample.add("i");
-        sample.add("psa");
+        //sample.add("kota");
+        //sample.add("i");
+        // sample.add("psa");
         System.out.println("po " + sample.size());
-        sample.remove("Ala");
+        sample.add("psa");
         System.out.println("po po" + sample.size());
-        System.out.println(sample.findFirst("psa"));
-       // System.out.println(sample.get(0));
+        System.out.println(sample.findFirst("Ala"));
+        sample.replace(0,-1);
+        System.out.println(sample.get(0));
 //        //dodajemy elementy typu Object - czyli dowolne, my dodamy ciągi znaków
 //        lista.add("Asia");
 //        lista.add("Basia");
@@ -45,5 +42,9 @@ public class Main{
 //        String imie = (String)lista.get(0);
 //        System.out.println("Pierwszy element listy to: "+imie);
 
+    }
+    public static void main(String[] args){
+
+        testFuction();
     }
 }
--- zad2/zad2_1/src/MyCustomList.java
+++ zad2/zad2_1/src/MyCustomList.java
@@ -3,42 +3,30 @@ import java.util.Arrays;
 /**
  * Created by kamil on 20.10.15.
  * lista:
- 1.add
- 2.remove
- 3.removeIndex
- 4.size
- 5.get
- 6.findFirst
- 7.
+ 1.add OK
+ 2.remove OK
+ 3.removeIndex OK
+ 4.size OK
+ 5.get OK
+ 6.findFirst OK
+ 7.replace
  */
 public class MyCustomList<T> {
 
-    private  T firstParameter;
     private T[] table, tempTable;
-    private int index;
-    private final int MAX = 50;
-    MyCustomList()
-    {
-        index = 0;
-        table = (T[])new Object[0];
-    }
-    public void set(T param)
-    {
-        firstParameter = param;
-    }
-   /* public T get()
-    {
-        return firstParameter;
-    }*/
+    MyCustomList() { table = (T[])new Object[0]; }
+
     public int size() {return table.length;}
+
     public void add(T object)
     {
         table = Arrays.copyOf(table, table.length+1);
-        table[index++] = object;
+        table[table.length-1] = object;
     }
+
     public T get(int paramIndex)
     {
-        if(paramIndex<index)
+        if(paramIndex<table.length)
             return table[paramIndex];
         else
             return null;
@@ -46,11 +34,29 @@ public class MyCustomList<T> {
 
     public void remove(T param)
     {
-        tempTable = (T[])new Object[index+1];
+        if(table.length == 0)
+            return;
+        tempTable = (T[])new Object[table.length];
         int j=0;
         for (int i=0; i< table.length; ++i)
         {
             if(table[i] != param)
+            {
+                tempTable[j] = table[i];
+                j++;
+            }
+        }
+        table=Arrays.copyOf(tempTable, j);
+    }
+    public void removeIndex(int param)
+    {
+        if(table.length == 0)
+            return;
+        tempTable = (T[])new Object[table.length];
+        int j=0;
+        for (int i=0; i< table.length; ++i)
+        {
+            if(i != param)
             {
                 tempTable[j] = table[i];
                 j++;
@@ -64,5 +70,16 @@ public class MyCustomList<T> {
             if(table[i] == param)
                 return i;
         return -1;
+    }
+    public boolean replace(int first, int second)
+    {
+        if (first >= table.length || second >= table.length)
+            return false;
+        else if(first <0 || second < 0)
+            return false;
+        T temp = table[first];
+        table[first] = table[second];
+        table[second] = temp;
+        return true;
     }
 }
